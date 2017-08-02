cc2e58b43ee6c7a71b0ee0ac5e6ecce2bed7f328
Makis Tzakis <gertzakis@gmail.com>
2017-08-02 22:02:21 +0300

    Initial Commit

---
+++ disk_usage.sh
@@ -0,0 +1,8 @@
+# Script for checking the disk_usage in your computer
+# This is free software, under the GNU v3 Public License
+# Author: Makis Tzakis
+# This is my first script so WARNING!
+
+
+echo "Hello"
+
