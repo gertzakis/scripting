065828c6ef1c5a3afdccbc164b2a3a9521d82bc5
Makis Tzakis <gertzakis@gmail.com>
2017-08-03 21:41:14 +0300

    Just print changes

--- disk_usage.sh
+++ disk_usage.sh
@@ -2,9 +2,9 @@
 
 # Script for find disk usage
 
-echo -e "\n"
+printf "\n"
 echo " ========== Disk Usage Script ========= "
 echo "Disk Usage per system: "
 df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1}'
-echo -e "\n\n"
+printf "\n\n"
 echo " ====================================== "
