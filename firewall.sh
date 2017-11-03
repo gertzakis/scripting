11c118059dc8f05daf32e2dd1ac68c79b22a9f6c
Gerasimos Tzakis <gertzakis@gmail.com>
2017-11-03 13:07:55 +0200

    Addind basic rules, like mysql pop, imap etc.

--- firewall.sh
+++ firewall.sh
@@ -13,6 +13,7 @@ iptables -P FORWARD DROP
 # Accept Established ssh & telnet.
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -p tcp --dport ssh -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -p tcp --dport telnet -j ACCEPT
+
 #
 # Accept only 1 ping per second. Faster than that gets dropped & logged.
 iptables -A INPUT -p icmp -m icmp --icmp-type echo-request -m limit --limit 1/second -j ACCEPT
@@ -20,12 +21,44 @@ iptables -A INPUT -p icmp -m icmp --icmp-type echo-request -j LOG
 iptables -A INPUT -p icmp -m icmp --icmp-type echo-request -j DROP
 
 #
-# Accept ongoing connections
+# Allow pop, imap and smtp
+iptables -A INPUT -p tcp --dport 25 -j ACCEPT
+iptables -A INPUT -p tcp --dport 465 -j ACCEPT
+iptables -A INPUT -p tcp --dport 143 -j ACCEPT
+iptables -A INPUT -p tcp --dport 993 -j ACCEPT
+iptables -A INPUT -p tcp --dport 587 -j ACCEPT
+iptables -A INPUT -p tcp --dport 110 -j ACCEPT
+iptables -A INPUT -p tcp --dport 995 -j ACCEPT
+
+#
+# Allow ongoing & local connections
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -s 127.0.0.1 -j ACCEPT
+
+#
+# Accept MySQL
+iptables -A INPUT -p tcp --dport 3306 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+
+#
+# Allow all outgoing
+iptables -A OUTPUT -m state --state NEW,ESTABLISHED -j ACCEPT
+
+#
+# Allow related incoming
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+#
+# Loopbacks
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
 
 #
 # Reject anything that is not established and passes at forward chain.
 iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -j REJECT
 
+# Allow anything outgoing.
+iptables -A OUTPUT -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
 
+# Reject everything else
+iptables -A INPUT -j DROP
